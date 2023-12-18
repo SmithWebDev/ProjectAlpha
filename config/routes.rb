86f2278d5b51e21265222ef77ee785d66131e867
Darrick Smith <smithwebdd@gmail.com>
2023-12-18 16:09:45 -0500

    [CHORE] Created pages controller and static pages for project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'pages/about'
+  get 'pages/services'
+  get 'pages/lover_letters'
+  get 'pages/portfolio'
+  get 'pages/blog'
+  get 'pages/connect'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
